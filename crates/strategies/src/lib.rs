a749389747f97478329af5a123126323c8a0c365
D-Pad <mcfincher29@gmail.com>
2026-02-17 00:02:25 -0600

    Started on strategies crate

--- crates/strategies/src/lib.rs
+++ crates/strategies/src/lib.rs
@@ -1,3 +1,25 @@
 pub use indicators::{self, *};
 
 
+pub struct Strategy {
+    name: String,
+    inputs: StrategyInputs
+}
+
+pub struct StrategyInputs {
+    moving_averages: Vec<MaInputs>
+}
+
+impl StrategyInputs {
+
+    pub fn new(
+        moving_averages: Vec<MaInputs>
+    ) -> Self {
+        Self {
+            moving_averages
+        }
+    }
+
+}
+
+
