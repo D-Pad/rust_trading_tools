63f9a4542a44760c681c22c6df7f2a7cd1b81c83
D-Pad <mcfincher29@gmail.com>
2026-02-17 22:47:48 -0600

    Added some doc strings to the 'strategies' crate

--- crates/strategies/src/lib.rs
+++ crates/strategies/src/lib.rs
@@ -7,11 +7,11 @@ pub enum StrategyInputError {
 }
 
 
-pub enum StrategyComponentType<'a> {
-    MA { ma_type: &'a str },
-}
-
-
+// ------------------------------- STRATEGY -------------------------------- //
+/// # Trading Strategy
+///
+/// A trading strategy stores a group of indicators, and uses their 
+/// calculations to determine entry and exit trading signals.
 pub struct Strategy {
     pub name: String,
     pub inputs: StrategyInputs
@@ -27,7 +27,25 @@ impl Strategy {
 }
 
 
+// -------------------------- TEMPLATE RENDERING --------------------------- //
+/// # Strategy Component 
+///
+/// Used to describe a strategy component. Can be used in the 
+/// StrategyInputs.add_new_default_component() method to add an indicator to 
+/// a strategy.
+pub enum StrategyComponentType<'a> {
+    MA { ma_type: &'a str },
+}
+
+
 #[derive(Serialize, Deserialize)]
+/// # Strategy Input Rendering
+///
+/// Used for creating and saving strategy templates for later use. This struct 
+/// only contains the inputs needed to initialize a Strategy, and does not 
+/// perform any indicator calculations on it's own. See the 
+/// 'add_new_default_component' method to see how components should be added
+/// to a strategy template.
 pub struct StrategyInputs {
     pub moving_averages: Option<Vec<MaInputs>>
 }
@@ -52,6 +70,21 @@ impl StrategyInputs {
     
     }
 
+    /// ## Add Default Indicator Component
+    ///
+    /// New indicator components can be added by passing a 
+    /// StrategyComponentType struct into this method, which is used to 
+    /// generate a new strategy template, but shouldn't have much control over 
+    /// the input values of the strategy. In other words, the default input 
+    /// values are rendered by default, and should be changed later via the 
+    /// TUI or web server.
+    ///
+    /// To add a component, a StrategyComponentType must be initialized first.
+    /// ```
+    /// let mut strat = Strategy::empty("My Strategy".to_string());
+    /// let comp = StrategyComponentType::MA { ma_type: "sma" };
+    /// strat.inputs.add_new_default_component(comp);
+    /// ```
     pub fn add_new_default_component(&mut self, comp: StrategyComponentType) 
         -> Result<(), StrategyInputError> {
         
